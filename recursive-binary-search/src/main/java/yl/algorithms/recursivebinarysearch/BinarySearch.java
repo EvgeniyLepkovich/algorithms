b4dfcbe1e1aeecd93468203c1441652d3014057f
Yauheniy Lepkovich <evgeniy.lepkovich@gmail.com>
2023-01-02 18:38:49 +0100

    implement recursive binary search

--- recursive-binary-search/src/main/java/yl/algorithms/recursivebinarysearch/BinarySearch.java
+++ recursive-binary-search/src/main/java/yl/algorithms/recursivebinarysearch/BinarySearch.java
@@ -1,2 +1,21 @@
-package yl.algorithms.recursivebinarysearch;public class BinarySearch {
+package yl.algorithms.recursivebinarysearch;
+
+public class BinarySearch {
+
+    public Integer intSearch(int[] array, int target) {
+        return intSearch(array, 0, array.length - 1, target);
+    }
+
+    private Integer intSearch(int[] array, int low, int high, int target) {
+        if (low > high) {
+            return null;
+        }
+
+        int mid = (low + high) / 2;
+        if (array[mid] == target) {
+            return mid;
+        }
+
+        return array[mid] > target ? intSearch(array, low, mid-1, target) : intSearch(array, mid+1, high, target);
+    }
 }
